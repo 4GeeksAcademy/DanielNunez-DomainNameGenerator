9e704716c4afb54fa5ea634227c5c95f0697c8bf
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-06-13 02:20:38 +0000

    DanielNunez_DomainNameGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function onload() {
+  let firstPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let firstAdj = adj[Math.floor(Math.random() * adj.length)];
+  let firstNoun = noun[Math.floor(Math.random() * noun.length)];
+  return `${firstPronoun}${firstAdj}${firstNoun}.com`;
+}
+document.getElementById("domain").innerText = onload();
